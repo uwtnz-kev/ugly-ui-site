bfb4c7226e0381fe07c53448bf580adc3d358493
uwtnz-kev <uwitonze.kevin@auf.edu.ph>
2025-11-05 10:21:59 +0800

    Create script.test.js
    Add Jest tests (passing and failing)

---
+++ script.test.js
@@ -0,0 +1,12 @@
+// script.test.js
+const { animateBackground } = require('./script');
+
+// ✅ Passing test
+test('animateBackground function should be defined', () => {
+  expect(animateBackground).toBeDefined();
+});
+
+// 🚫 Failing test (we’ll fix this later)
+test('intentional failure test', () => {
+  expect(2 + 2).toBe(5);
+});
